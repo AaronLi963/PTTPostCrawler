40ea42d0ffb39aa0cb56faf3b60ac97223e98b0c
AaronLi963 <zy.li0824@gmail.com>
2021-01-09 00:31:47 +0800

    scroll to bottom

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from PyPtt import PTT
 import util
 import json
 import time
+import logging
 
 
 class MainWindow(QMainWindow, ui.Ui_MainWindow):
@@ -17,12 +18,20 @@ class MainWindow(QMainWindow, ui.Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle("PTT推文顯示器")
         # self.setFixedSize(960, 840)
-        self.bottonLogin.clicked.connect(self.loginPTT)
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar) 
         self.msgBox = QMessageBox()
         self.setUserInfo()
         self.labelBuffer = []
+        self.dataThread = DataThread()
+        self.bottonLoadPosts.clicked.connect(self.getPosts)
+        # set scroll area
+        self.scrollPost.setWidgetResizable(True)
+        self.scrollPost.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.postWidget = QWidget()
+        self.scrollLayout = QVBoxLayout()
+        self.postWidget.setLayout(self.scrollLayout)
+        self.scrollPost.setWidget(self.postWidget)
 
     def setUserInfo(self):
         try:
@@ -33,65 +42,40 @@ class MainWindow(QMainWindow, ui.Ui_MainWindow):
                 self.inputPTTAID.setText(userInfo.get("aid",""))
         except:
             pass
+    
+    def setStatus(self, status):
+        self.statusBar.showMessage(status,5000)
 
-    def loginPTT(self):
-        pttID = self.inputPTTID.text()
-        pttPW = self.inputPTTPassword.text()
-        if pttID == "" or pttPW == "":
-            self.statusBar.showMessage("請輸入帳號密碼", 5000)
-            return
+    def setTitle(self, title):
+        self.textArticleTitle.setText(title)
+    
+    def addPost(self, post):
+        label = QLabel(post)
+        label.adjustSize()
+        self.scrollLayout.addWidget(label)
+        scroll_bar = self.scrollPost.verticalScrollBar()
+        scroll_bar.rangeChanged.connect(lambda: scroll_bar.setValue(scroll_bar.maximum()))
 
-        try: 
-            self.statusBar.showMessage("登入中")
-            ptt_bot.login(pttID, pttPW)
-        except PTT.exceptions.LoginError:
-            self.msgBox.setText("登入失敗")
-            self.msgBox.show()
-        except PTT.exceptions.WrongIDorPassword:
-            self.msgBox.setText("帳號或密碼錯誤")
-            self.msgBox.show()
-        except PTT.exceptions.LoginTooOften:
-            self.msgBox.setText("請稍後再登入")
-            self.msgBox.show()
-        except Exception : 
-            self.msgBox.setText("unknown error")
-            self.msgBox.show()
-        else :
-            self.statusBar.showMessage("登入成功: " + pttID, 5000)
-            self.bottonLoadPosts.clicked.connect(self.getPosts)
-     
     def getPosts(self): # trigger DataThread to get data
+        self.bottonLoadPosts.clicked.disconnect()
         aid, board = util.ParseAID(self.inputPTTAID.text())
-        post_info = ptt_bot.get_post(board, post_aid=aid)
-        self.textArticleTitle.setText(post_info.title)
-        self.scrollPost.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-        self.postWidget = QWidget()
-        self.scrollLayout = QVBoxLayout()
-        if len(post_info.push_list) > 15:
-            post_info.push_list = post_info.push_list[-15:]
-        for push_info in post_info.push_list:
-            pushType = '推'
-            if push_info == PTT.data_type.push_type.BOO:
-                pushType = '噓'
-            elif push_info == PTT.data_type.push_type.ARROW:
-                pushType = '->'
-            author = push_info.author
-            content = push_info.content
-            buffer = pushType + ' ' + author + '\n  ' + content
-            label = QLabel(buffer)
-            label.adjustSize()
-            self.scrollLayout.addWidget(label)
-        self.postWidget.setLayout(self.scrollLayout)
-        self.scrollPost.setWidget(self.postWidget)
+        self.dataThread.statusSignal.connect(self.setStatus)
+        self.dataThread.titleSignal.connect(self.setTitle)
+        self.dataThread.postSignal.connect(self.addPost)
+        id = self.inputPTTID.text()
+        password = self.inputPTTPassword.text()
+        self.dataThread.setting(id, password, board, aid)
+        self.dataThread.start()
+        
 
 class DataThread(QThread): # ref https://www.cnblogs.com/linyfeng/p/12239856.html
     statusSignal = pyqtSignal(str)
     titleSignal = pyqtSignal(str)
     postSignal = pyqtSignal(str)
+    ptt_bot = PTT.API()
 
-    def __init__(self,board,aid,index):
+    def __init__(self):
         super().__init__()
-        self.ptt_bot = PTT.API()
         self.index = 0
     
     def setting(self, id, password, board, aid):
@@ -110,8 +94,10 @@ class DataThread(QThread): # ref https://www.cnblogs.com/linyfeng/p/12239856.htm
             status = "帳號或密碼錯誤"
         except PTT.exceptions.LoginTooOften:
             status = "請稍後再登入"
-        except Exception : 
+        except Exception as e: 
             status = "unknown error"
+            error_class = e.__class__.__name__
+            logging.warning(str(e))
         else :
             status = "登入成功: " + self.id
             util.SaveUserJson(self.id, self.password, self.board, self.aid)
@@ -122,10 +108,10 @@ class DataThread(QThread): # ref https://www.cnblogs.com/linyfeng/p/12239856.htm
         return False
     
     def run(self):
+        self.ptt_bot = PTT.API(log_level=PTT.log.level.INFO)
         loginSuccess = self.login()
         if not loginSuccess:
             return
-
         # start to get posts
         while True :
             post_info = self.ptt_bot.get_post(self.board, post_aid=self.aid)
@@ -133,8 +119,10 @@ class DataThread(QThread): # ref https://www.cnblogs.com/linyfeng/p/12239856.htm
             totalPosts = len(post_info.push_list)
             if totalPosts <= self.index:
                 continue
+            if totalPosts - self.index > 30:
+                self.index = totalPosts - 30
             newPosts = post_info.push_list[self.index:]
-            for push_info in post_info.push_list:
+            for push_info in newPosts:
                 floor = self.index + 1
                 pushType = '推'
                 if push_info == PTT.data_type.push_type.BOO:
@@ -143,10 +131,10 @@ class DataThread(QThread): # ref https://www.cnblogs.com/linyfeng/p/12239856.htm
                     pushType = '->'
                 author = push_info.author
                 content = push_info.content
-                buffer = floor + 'F:  ' + pushType + ' ' + author + '\n  ' + content
-                self.postSignal.emit(buffer)
+                buffer = str(floor) + 'F:  ' + pushType + ' ' + author + '\n  ' + content
                 self.index = floor
-            time.sleep(3) # load new posts every 3 seconds
+                self.postSignal.emit(buffer)
+            time.sleep(1) # load new posts every 1 second
 
 
 
